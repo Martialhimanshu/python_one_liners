70518546d1fca20f147c60d5f557a6f814f5b1ca
Martial Himanshu <martial.msit@gmail.com>
2020-11-25 03:29:09 +0530

    Update 2_Math.py
    Added 'f' expression

--- 2_Math.py
+++ 2_Math.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 x,y = 3,2
-print("If x=3, y=2")
-print("x/y is: ", x/y)
+print(f"If x={x}, y={y}")
+print(f"x/y is: {x/y}")
 
 x,y = 100,9
 
-print("If x,y = 100,9")
-print("int(x)/int(y) is ", int(x)/int(y))
+print(f"If x,y = {x},{y}")
+print(f"int(x)/int(y) is {int(x)/int(y)}")
 
-print("float x/y = ", float(x)/y)
-
+print(f"float x/y = {float(x)/y}")
